02dd37ef321b55c57d2a3753cd39ecb41114d1e4
Elec2PCB Tech-Solutions <49590651+Elec2PCB@users.noreply.github.com>
2019-04-13 21:45:24 +0700

    Add files via upload

--- Day 3 - Timers/Ticker.cpp
+++ Day 3 - Timers/Ticker.cpp
@@ -1,20 +1,19 @@
 //Khóa học Lập trình STM32
-//Bài: TimeOut
+//Bài: Ticker
 //MCU: STM32F103R8T6
 //lpson @ Elec2PCB.com
 //Email: elec2pcb@gmail.com
 //Mobile: 0905-912-019
 #include "mbed.h"
-Timeout flipper;
-DigitalOut led1(PA_8);
-DigitalOut led2(PD_2);
+Ticker flipper;
+DigitalOut led1(PD_2);
+DigitalOut led2(PA_8);
 void flip() {
     led2 = !led2;
 }
 int main() {
     led2 = 1;
-    flipper.attach(&flip, 2.0); // Gọi hàm flip sau 2s
-    // spin in a main loop. flipper will interrupt it to call flip
+    flipper.attach(&flip, 2.0); //sau mỗi 2s gọi hàm flip một lần
     while(1) {
         led1 = !led1;
         wait(0.2);
